f206be08537dcdfabcf8ed055f372c6e4e185def
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2024-10-09 12:25:40 +0530

    add: health route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div className="h-screen w-full flex flex-col justify-center items-center">
-      <h1 className="text-center text-4xl font-bold text-emerald-600">FlatBuddy Backend</h1>
-      <p className="text-2xl font-semibold text-muted-foreground">BuildingFullThrotle LLC</p>
+      <h1 className="text-center text-3xl font-semibold">FlatBuddy Backend</h1>
+      <p className="text-muted-foreground text-center">BuildingFullThrotle LLC</p>
     </div>
   );
 }
