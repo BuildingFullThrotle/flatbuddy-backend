1555c087bf41236c9b88693b7b1f4b06e14b63ff
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2024-10-09 12:15:30 +0530

    fix: unused variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="h-screen w-full flex flex-col justify-center items-center">
-      <h1 className="text-center text-4xl font-bold">FlatBuddy Backend</h1>
+      <h1 className="text-center text-4xl font-bold text-emerald-600">FlatBuddy Backend</h1>
       <p className="text-2xl font-semibold text-muted-foreground">BuildingFullThrotle LLC</p>
     </div>
   );
